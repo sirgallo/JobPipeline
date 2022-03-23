39c790b4bf4e3136ceaf5edd1bccdae860397446
sirgallo <nickgallo97@gmail.com>
2022-03-22 22:02:37 -0400

    update commentary

--- core/providers/LoadBalanceProvider.ts
+++ core/providers/LoadBalanceProvider.ts
@@ -86,6 +86,14 @@ export class LoadBalanceProvider {
     }
   }
 
+  /*
+    Client facing socket that picks up requests from the gateway apis. Requests are then fed into the job pipeline
+    and fed into the worker router. Any number of clients can connect to the client side.
+
+    The load balancer saves client ids in a map and then heartbeats every system in the map to validate that it is alive.
+    If the system fails, it is removed from the map of validated systems. Responses from the worker machines are routed
+    to a randomly selected available client machine
+  */
   async startClientRouter() {
     this.clientsock.routingId = randomUUID(cryptoOptions)
     
@@ -123,6 +131,11 @@ export class LoadBalanceProvider {
     }
   }
 
+  /*
+    Requests on job queue are picked up by worker router and distributed to worker machines. Machines are picked 
+    randomly from a pool of available machines, with status: 'Ready' .This evenly distributes work to available 
+    machines. Any number of workers can connect to the worker facing load balancer.
+  */
   async startWorkerRouter() {
     this.workersock.routingId = randomUUID(cryptoOptions)
 
@@ -156,6 +169,7 @@ export class LoadBalanceProvider {
     }
   }
 
+  //  handle randomly distributing jobs on new job event and stale jobs
   private jobQueueOn() {
     this.jobQueue.queueUpdate.on(this.jobQueue.eventName, async () => {
       if (this.jobQueue.getQueue().length > 0) {
@@ -164,7 +178,7 @@ export class LoadBalanceProvider {
 
         try {
           if (job.jobId) { 
-            const index = this.availableMachines(this.knownWorkersMap)
+            const index = this.selectMachine(this.knownWorkersMap)
             //  need to pass identity in first frame
             //  router stores id of dealer in hash table
             await this.workersock.send([ 
@@ -179,13 +193,14 @@ export class LoadBalanceProvider {
     })
   }
 
+  //  handle randomly distributing a response to a gateway machine
   private retQueueOn() {
     this.retQueue.queueUpdate.on(this.retQueue.eventName, async () => {
       if (this.retQueue.getQueue().length > 0) {
         const returnObj = JSON.stringify(this.retQueue.pop())
 
         try {
-          const index = this.availableMachines(this.knownClientsMap)
+          const index = this.selectMachine(this.knownClientsMap)
           await this.clientsock.send([
             [...this.knownClientMachines][index],
             returnObj
@@ -197,10 +212,23 @@ export class LoadBalanceProvider {
     })
   }
 
+  /*
+    Heartbeat:
+
+      LB selects all machines on the known machines dictionary and checks if the validation date is within the 
+      timeframe.
+
+      A heartbeat is sent to all machines and a response is returned.
+
+      The response is handled in the socket event listener for both the client and backend facing sockets
+
+      On response, the map is updated to reflect the machine health
+  */
   private async heartbeat() {
     this.lbLog.info(`Sleep on Startup for: ${ONSTARTUP}s`)
     await sleep(toMs.sec(ONSTARTUP))
     this.lbLog.info('Begin Heartbeating...')
+    
     while (true) {
       for (const machine of this.getMachines(this.knownWorkersMap)) {
         await this.workersock.send([
@@ -218,24 +246,29 @@ export class LoadBalanceProvider {
     }
   }
 
-  private availableMachines(machines: Record<string, IAvailableMachine>): number {
+  //  Get a random machine index from the available machines
+  private selectMachine(machines: Record<string, IAvailableMachine>): number {
     const totalAvailableMachines = Object.keys(machines)
       .map(key => machines[key].status === 'Ready' ? key : null)
-      .filter(el => el).length
+      .filter(el => el)
+      .length
+
     const randomValue = Math.random() * totalAvailableMachines
     const roundedIndex = Math.floor(randomValue)
     
     return roundedIndex
   }
 
+  //  Return a list of previously validated machines
   private getMachines(machines: Record<string, IAvailableMachine>): Array<string> {
     const machinesToCheck = Object.keys(machines)
-      .map( key => this.outsideTimeout(machines[key].validated, INTERVAL) ? key : null)
+      .map(key => this.outsideTimeout(machines[key].validated, INTERVAL) ? key : null)
       .filter(el => el)
     
     return machinesToCheck
   }
 
+  //  Check the previous validation date against the current date
   private outsideTimeout(dateToTest: Date, timeout: number): boolean {
     const now = new Date()
     const pastTimeout = dateToTest.getTime() + timeout
